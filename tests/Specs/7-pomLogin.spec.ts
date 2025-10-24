4e8b7d21df1646afb276d78375202e49b9d836a0
djelidrh99 <djelida871@gmail.com>
2025-10-24 17:25:56 +0000

    second commit

--- tests/Specs/7-pomLogin.spec.ts
+++ tests/Specs/7-pomLogin.spec.ts
@@ -12,23 +12,23 @@ let parseJSON = JSON.parse(JSON.stringify(jsonData));
 test.beforeEach(async ({ page }) => {
       console.log(parseJSON.userName);
     pageManager = new poManger(page);
-    // await pageManager.gotoLoginPage().goto();
+    await pageManager.gotoLoginPage().goto();
 
 })
 
 
-test.describe('Login tests', () => {
+test.describe('Login tests @login', () => {
 
     test("valid login" , async ({page})=>{
-        // (await pageManager.gotoLoginPage()).login(parseJSON.userName, parseJSON.password);
+        (await pageManager.gotoLoginPage()).login(parseJSON.userName, parseJSON.password);
         await pageManager.gotoHomePage().expectWelcomeMessage();
         page.pause();
 
     })
 
 
-    // test("invalid login" , async ({page})=>{
-    //     (await pageManager.gotoLoginPage()).login('Admin', 'admin12d');
-    //     await pageManager.gotoLoginPage().expectInvalidLogin();
-    // })
+    test("invalid login" , async ({page})=>{
+        (await pageManager.gotoLoginPage()).login('Admin', 'admin12d');
+        await pageManager.gotoLoginPage().expectInvalidLogin();
+    })
 })
